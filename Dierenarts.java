4f67754519a676c9266fca491cad33ce5a5c6b03
DaniedBatWS <d.debackker@workingspirit.nl>
2019-05-10 14:31:40 +0200

    fix A

--- Dierenarts.java
+++ Dierenarts.java
@@ -1,6 +1,6 @@
 package dierenarts;
 
-public class Dierenarts {
+public class DierenArts {
 	void onderzoeken() {
 		System.out.println("Het dier is ziek, ik ga beoordelen wat er aan de hand is.");
 	}	
