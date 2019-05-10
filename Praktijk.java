edbb0b20a7195d01b85ceaa1486c7ca1b8082cf7
ANekoui <a.nekoui@workingspirit.nl>
2019-05-10 15:37:00 +0200

    Commit

--- Praktijk.java
+++ Praktijk.java
@@ -1,11 +1,24 @@
 package dierenarts;
 
+import java.util.ArrayList;
+
 public class Praktijk {
 	
+	Assistent assistent1 = new Assistent();
+	ArrayList<Dier> dieren = new ArrayList<>();
+	
+	void openen() {
+		
+		System.out.println("Welkom bij het praktijk");
+		dieren.add(Assistent.aanmelden("Pietje", true, 500));
+		dieren.add(Assistent.aanmelden("Ham", false, 78));
+		dieren.add(Assistent.aanmelden("IJs", true, 8));
+		dieren.add(Assistent.aanmelden("Beer", true, 52));
+			
+	}
+	
 	void consultatie() {
-		
-		
-		
+			
 	}
 
 }
