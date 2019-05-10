61275d36d130bc9cc9eb9e41f71490c627f2f126
ANekoui <a.nekoui@workingspirit.nl>
2019-05-10 14:19:05 +0200

    lets go

--- DierenartsApp.java
+++ DierenartsApp.java
@@ -3,10 +3,10 @@ package dierenarts;
 public class DierenartsApp {
 	public static void main(String[] args) {
 		System.out.println("hoi");
-		new DierenArts.onderzoeken();
-		new Dier.lopen();
-		new Assistent.assisteren();
-		new Kassa.rinkelen();
+		new DierenArts().onderzoeken();
+		new Dier().lopen();
+		new Assistent().assisteren();
+		new Kassa().rinkelen();
 	
 		
 		
