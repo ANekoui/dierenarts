e99250a1c6b964b927d3a474e08988d274ad670d
GhisJava <g.fardaoussi@workingspirit.nl>
2019-05-10 15:44:23 +0200

    afrekenen en rinkelen

--- Kassa.java
+++ Kassa.java
@@ -1,9 +1,20 @@
 package dierenarts;
 
+class Kassa extends Assistent {
+	int operatiePrijs;
+	String dierSoort;
 
-class Kassa{
 	void rinkelen() {
-		System.out.println();
-		
+		System.out.println("Kassa rinkelt");
+	}
+
+	void afrekenen(Dier hetDier, int operatiePrijs) {
+		rinkelen();
+		System.out.println("Welkom bij de kassa: de operatie kost " + operatiePrijs);
+		if (operatiePrijs > hetDier.geld) {
+			System.out.println("Geld is niet genoeg: ga de instrumenten maar afwassen.");
+		} else if (operatiePrijs >= hetDier.geld || operatiePrijs <= hetDier.geld) {
+			System.out.println("Hier heb je de bon en veel beterschap gewenst.");
+		}
 	}
 }
