67736a559ed875c5ccbbb6b651253648a03f4da9
Frankkeijzer <f.keijzer@workingspirit.nl>
2019-05-10 14:43:55 +0200

    Tekst bij methode

--- Assistent.java
+++ Assistent.java
@@ -2,6 +2,6 @@ package dierenarts;
 
 public class Assistent {
 	void assisteren() {
-		
+		System.out.println("Ik assisteer de dierenarts wel even met het zieke dier, hij/zij kan het niet alleen");
 	}
 }
