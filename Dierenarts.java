b6c611f76156d18bcd6a40d8fb5a5d3e00389fa2
ANekoui <a.nekoui@workingspirit.nl>
2019-05-10 14:45:39 +0200

    a

--- Dierenarts.java
+++ Dierenarts.java
@@ -1,6 +1,6 @@
 package dierenarts;
 
-public class DierenArts {
+public class Dierenarts {
 	void onderzoeken() {
 		System.out.println("Het dier is ziek, ik ga beoordelen wat er aan de hand is.");
 	}	
--- DierenartsApp.java
+++ DierenartsApp.java
@@ -3,7 +3,7 @@ package dierenarts;
 public class DierenartsApp {
 	public static void main(String[] args) {
 		System.out.println("hoi");
-		new DierenArts().onderzoeken();
+		new Dierenarts().onderzoeken();
 		new Dier().lopen();
 		new Assistent().assisteren();
 		new Kassa().rinkelen();
