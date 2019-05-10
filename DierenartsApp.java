78aaa79a5a79fb981ab59522eb7d0a6baced524d
ANekoui <a.nekoui@workingspirit.nl>
2019-05-10 14:14:51 +0200

    second commit

--- DierenartsApp.java
+++ DierenartsApp.java
@@ -1,5 +1,15 @@
 package dierenarts;
 
 public class DierenartsApp {
+	public static void main(String[] args) {
+		System.out.println("hoi");
+		new DierenArts.onderzoeken();
+		new Dier.lopen();
+		new Assistent.assisteren();
+		new Kassa.rinkelen();
+	
+		
+		
+	}
 
 }
